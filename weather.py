3e3bcdd8d2c55e265a127c6ea3561d4580305f9f
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-11-21 15:25:02 +0100

    add clé

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 
-API_KEY = "a678259620c3934dfe35214c28802c2a"
+API_KEY = "b463363b67496203f845ecd8ede6e02a"
 city_1 = "Toulouse"
 city_2 = "Saint-Geours-de-Maremne"
 BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
