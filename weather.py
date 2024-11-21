4923884be53937e4749d559e9b38bf8c473fa9df
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-11-21 15:12:17 +0100

    add docstring

--- weather.py
+++ weather.py
@@ -8,24 +8,16 @@ BASE_URL = "http://api.openweathermap.org/data/2.5/forecast"
 
 
 def get_weather(city):
-    params = {
-        "q": city,
-        "appid": API_KEY,
-        "units": "metric",
-    }
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"error to get {city}: {response.status_code}")
-        return None
+    """
+    Fetch the weather forecast for a given city from the OpenWeatherMap API.
 
+    Args:
+        city (str): The name of the city for which the weather forecast is requested.
 
-forecast_toulouse = get_weather(city_1)
-forecast_saint_geours = get_weather(city_2)
-
-
-def get_weather(city):
+    Returns:
+        dict: The weather forecast data in JSON format if the request is successful,
+              or None if the request fails.
+    """
     params = {
         "q": city,
         "appid": API_KEY,
@@ -44,6 +36,15 @@ forecast_saint_geours = get_weather(city_2)
 
 
 def temperatures(weather):
+    """
+    Extracts the minimum and maximum temperatures for the next 5 days from the weather data.
+
+    Args:
+        weather (dict): The weather forecast data for a city in JSON format.
+
+    Returns:
+        dict: A dictionary with the dates as keys and the corresponding minimum and maximum temperatures.
+    """
     daily_forecast = {}
     today = datetime.now().strftime("%Y-%m-%d")
     end_date = (datetime.now() + timedelta(days=5)).strftime("%Y-%m-%d")
